60d92a7087a5b7ea2d54e9d26ef99b4e9ed93683
綸麻 山口 <rapisu_re_raito8668@au.com>
2023-11-30 14:04:08 +0900

    add data.cpp

---
+++ data.h
@@ -0,0 +1,4 @@
+#pragma once
+#include<stdio.h>
+void loadData(FILE* fp,bool* flag);
+void printData(char* data,int row,int col,bool* flag);
--- main.cpp
+++ main.cpp
@@ -1,46 +1,8 @@
 #include<stdio.h>
-#include<assert.h>
+#include"data.h"
 int main() {
 	bool flag = 1;
-	FILE* fp;
-	fopen_s(&fp, "data.txt", "r");
-	assert(fp != 0);
-	fseek(fp, 0, SEEK_END);
-	int fileSize = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	char* data = new char[fileSize];
-	fread(data, sizeof(char), fileSize, fp);
-	fclose(fp);
-	int row = 0;
-	int col = 0;
-	int count = 0;
-	for (int i = 0; i < fileSize; i++) {
-		count++;
-		if (data[i] == '\n') {
-			if (row == 0) {
-				col = count;
-			}
-			else if (col != count) {
-				printf("—ñ”‚ª‚»‚ë‚Á‚Ä‚È‚¢‚æ");
-				flag = 0;
-			}
-			row++;
-			count = 0;
-		}
-	}
-	if (fileSize % row != 0) {
-		printf("s”‚ª‚»‚ë‚Á‚Ä‚È‚¢‚æ");
-		flag = 0;
-	}
-	if (flag) {
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (data[j + i * col] == '1')printf("–");
-				else if (data[j + i * col] == '0')printf("@");
-			}
-			printf("\n");
-		}
-	}
-	delete[] data;
+	FILE* fp = nullptr;
+	loadData(fp, &flag);
 	return 0;
 }
